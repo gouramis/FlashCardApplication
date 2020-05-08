b782e69da3099dcf6c41d5a3a82f2d7d73537bb4
Cameron Fitzpatrick <49736473+gouramis@users.noreply.github.com>
2020-05-08 13:28:43 -0700

    Update createDB.js

--- createDB.js
+++ createDB.js
@@ -1,25 +1,25 @@
-// Globals
-const sqlite3 = require("sqlite3").verbose();  // use sqlite
-const fs = require("fs"); // file system
-
+//Created by Cameron Fitzpatrick 2019
+//Modified 2020
+/* 
+   This file creates a SQLite3 DataBase
+*/
+//require sqlite3 and filesystem:
+const sqlite3 = require("sqlite3").verbose();
+const fs = require("fs");
+//filename:
 const dbFileName = "Flashcards.db";
 // makes the object that represents the database in our code
-const db = new sqlite3.Database(dbFileName);  // object, not database.
-
-// Initialize table.
-// If the table already exists, causes an error.
-// Fix the error by removing or renaming Flashcards.db
-//creating a column userID to hold google ID
-const cmdStr = 'CREATE TABLE Flashcards (userid TEXT, english TEXT, malay TEXT, seen INT, correct INT)';
-const cmdStr1 = 'CREATE TABLE Users (googleid TEXT PRIMARY KEY, firstName TEXT, lastName TEXT)';
-/*
-||userID|firstName|lastName|english|malay|seen|correct||
-*/
-db.run(cmdStr);
-db.run(cmdStr1);
-// Always use the callback for database operations and print out any
-// error messages you get.
-// This database stuff is hard to debug, give yourself a fighting chance.
+const db = new sqlite3.Database(dbFileName);
+//Initialize table.
+//If the table already exists, causes an error.
+//Fix the error by removing or renaming Flashcards.db
+//Creating a column userID to hold google ID
+const cmdTable1 = 'CREATE TABLE Flashcards (userid TEXT, english TEXT, malay TEXT, seen INT, correct INT)';
+const cmdTable2 = 'CREATE TABLE Users (googleid TEXT PRIMARY KEY, firstName TEXT, lastName TEXT)';
+//run commands:
+db.run(cmdTable1);
+db.run(cmdTable2);
+//Custom callback function to errcheck db call:
 function tableCreationCallback(err) {
     if (err) {
 	console.log("Table creation error",err);
