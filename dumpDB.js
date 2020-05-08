a5d5623779250b1e18f909fbd07b23757161c027
Cameron Fitzpatrick <49736473+gouramis@users.noreply.github.com>
2020-05-08 13:29:53 -0700

    Update dumpDB.js

--- dumpDB.js
+++ dumpDB.js
@@ -1,10 +1,13 @@
+//Created by Cameron Fitzpatrick 2019
+//Modified 2020
+/* 
+   This file dumps the db:
+*/
 const sqlite3 = require("sqlite3").verbose(); 
 const fs = require("fs"); // file system
-
 const dbFileName = "Flashcards.db";
 // makes the object that represents the database in our code
 const db = new sqlite3.Database(dbFileName);
-
 function dumpDB(){
     db.all ( 'SELECT * FROM flashcards', dataCallback);
     function dataCallback( err, data ) {console.log(data)}
