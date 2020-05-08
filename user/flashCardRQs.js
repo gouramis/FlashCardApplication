7a173bb422339a8ef95bf74ecba82a0efc08bc21
Cameron Fitzpatrick <49736473+gouramis@users.noreply.github.com>
2020-05-08 13:49:06 -0700

    Update flashCardRQs.js

--- user/flashCardRQs.js
+++ user/flashCardRQs.js
@@ -1,11 +1,11 @@
-
-//this started as the cors server but not it's doing ajax requests
-//but I have used it so much I don't want to change it's name because i know it's ajax
+//Created by Cameron Fitzpatrick 2019
+//Modified 2020
 export function createAJAXRequest(method, url) {
     let xhr = new XMLHttpRequest();
     xhr.open(method, url, true);  // call its open method
     return xhr;
 }
+
 function getUrlVars() {
 let vars = {};
 let parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
@@ -15,14 +15,14 @@ function(m,key,value) {
 return vars;
 }
 //above function from internet, all over forgot exactly where... think SO or something - it just helps me get the urlT
-//this is the only thing I brought in from an outside source- other than what was given to us to use.
+//this is the only thing I brought in from an outside source
 
 export function makeTranslateRequest(word = getUrlVars()(["english"])) {
     let url1 = "/user/translate?english=";
     //let fType = getUrlVars()["english"];
     let url2 = url1 + word;
     let xhr = createAJAXRequest('GET', url2);
-//checking for errors
+	//checking for errors
     if (!xhr) {
 	     alert('CORS not supported');
 	     return;
@@ -45,7 +45,7 @@ export function makeGetNameRequest(){
 	     console.log(object);
     }
     xhr.send();
-}//can make a get seen the same way, and a get correct the same way
+}
 export function makeIncrementSeenRequest(card){
   let base = '/user/incrementSeen?cardID=';
   let url = base + card
@@ -53,7 +53,6 @@ export function makeIncrementSeenRequest(card){
   xhr.onload = function() {
     let responseStr = xhr.responseText;  // get the JSON string
     console.log(responseStr);
-    //let object = JSON.parse(responseStr);
   }
   xhr.send();
 }
@@ -64,7 +63,6 @@ export function makeIncrementCorrectRequest(card){
   xhr.onload = function() {
     let responseStr = xhr.responseText;  // get the JSON string
     console.log(responseStr);
-    //let object = JSON.parse(responseStr);
   }
   xhr.send();
 }
@@ -80,55 +78,27 @@ export function makeCheckStatusRequest(){
   }
   xhr.send();
 }
-
 export function makeStoreRequest(word = getUrlVars()["english"], translation = getUrlVars()["malay"]) {
     let url1 = "store?english=";
     let url2 = url1 + word + "&malay=" + translation;
     let xhr = createAJAXRequest('GET', url2);
-
     //checking if browser does CORS
     if (!xhr) {
 	     alert('CORS not supported');
 	     return;
     }
-    /*
-       xhr.onload = function() {
-	     let responseStr = xhr.responseText;  // get the JSON string
-	     let object = JSON.parse(responseStr);  // turn it into an object
-	     document.getElementById("outputGoesHere").textContent = (JSON.stringify(object, undefined, 2));
-    }*/
     xhr.send();
-}/*
-let input = document.getElementById("inputEng");
-window.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("inputEng").addEventListener("keyup", function(event) {
-      if (event.keyCode === 13) {
-        sbm();
-      }
-    });
-});
-input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    sbm();
-  }
-})*/
+}
 export function sbm(){
   //grab input and make request because the user has clicked "enter"
     let id = document.getElementById("inputEng").value;
     makeTranslateRequest(id);
 }
 export function save(){
-  //store in db
     let id = document.getElementById("inputEng").value;
     let malay = document.getElementById("outputGoesHere").textContent;
-    /*console.log(translatedText);
-
-    obj = JSON.parse(translatedText);
-    let malay = obj.InMalay;*/
-
-    makeStoreRequest(id, malay);
+	makeStoreRequest(id, malay);
 }
-
 export function seen(card){
   makeIncrementSeenRequest(card);
   console.log(card);
