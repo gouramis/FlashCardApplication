1f39ef7043b34ae7c474add1679e4726bf3f244c
Cameron Fitzpatrick <49736473+gouramis@users.noreply.github.com>
2020-05-08 13:44:24 -0700

    Update getUsername.js

--- getUsername.js
+++ getUsername.js
@@ -1,16 +1,16 @@
+//Created by Cameron Fitzpatrick 2019
 const sqlite3 = require("sqlite3").verbose();
 const dbFileName = "Flashcards.db";
-// makes the object that represents the database in our code
+//makes the object that represents the database in our code
 //this is the interaction between the code and the database
 const db = new sqlite3.Database(dbFileName);
-
 function getFirstName(req, res, next) {
   let userTag = req.user.userData.googleid;
   let stuff = 'SELECT * FROM Users WHERE googleid = ?';
   db.get(stuff, userTag, callBackFunSChION);
   function callBackFunSChION (err, rowData) {
     if (err) {
-      console.log("idk");
+      console.log(err);
     } else {
       let userData = {userData: rowData};
       username = rowData.firstName;
