1ec95bf836d65b9c37750d33277f98be289e2b83
Cameron Fitzpatrick <49736473+gouramis@users.noreply.github.com>
2020-05-08 13:43:36 -0700

    Update getCardsFromDB.js

--- getCardsFromDB.js
+++ getCardsFromDB.js
@@ -1,12 +1,12 @@
+//Created by Cameron Fitzpatrick 2019
+//Modified 2020
 const sqlite3 = require("sqlite3").verbose();
 const dbFileName = "Flashcards.db";
 // makes the object that represents the database in our code
 //this is the interaction between the code and the database
 const db = new sqlite3.Database(dbFileName);
-
 function getCards(req, res, next) {
   let userTag = req.user.userData.googleid;
-  console.log("user id from ceck req" + userTag);
   let stuff = 'SELECT * FROM Flashcards WHERE userid = ?';
   db.all(stuff, userTag, callBackFun);
   function callBackFun (err, arrayData) {
@@ -23,9 +23,7 @@ function getCards(req, res, next) {
         console.log("not in db");
         res.json({ cards : false, rows: [] });
       }
-
     }
   }
 }
-
 exports.getCards = getCards;
